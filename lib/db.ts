6048ab594ead8676755da4f98e914e27805dbceb
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-11-15 22:34:09 +0530

    db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -7,3 +7,35 @@ if(!MONGODB_URI) {
         "Please define the MONGODB_URI environment variable inside .env.local"
     );
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
